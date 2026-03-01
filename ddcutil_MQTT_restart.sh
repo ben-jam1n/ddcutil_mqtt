4bfef3686cfb3b413c0c71e3329e4ff078d5680e
Ben <ben@benaddie.com>
2026-03-01 13:33:24 -0600

    updated gitignore amd restart service

--- ddcutil_MQTT_restart.sh
+++ ddcutil_MQTT_restart.sh
@@ -2,9 +2,7 @@
 # ddcutil_MQTT_restart.sh - Helper script to restart the ddcutil_MQTT service
 # This script is called by the ddcutil_MQTT_restart.service systemd unit
 
-INSTALL_DIR="/opt/ddcutil_MQTT"
-
-# Restart the main service
-sudo systemctl restart ddcutil_MQTT.service
+# Restart the main service (already running as root, no sudo needed)
+systemctl restart ddcutil_MQTT.service
 
 exit $?
