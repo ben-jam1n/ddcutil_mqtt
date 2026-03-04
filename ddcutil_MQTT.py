b963b21303167648e2710b2447aa2a64275e3bc0
Ben <ben@benaddie.com>
2026-03-03 20:47:42 -0600

    Added support URL back to device level

--- ddcutil_MQTT.py
+++ ddcutil_MQTT.py
@@ -209,6 +209,7 @@ def main():
                 "manufacturer": "ben-jam1n",
                 "model": "ddcutil to MQTT",
                 "configuration_url": f"http://{get_local_ip()}",
+                "support_url": "https://github.com/ben-jam1n/ddcutil_mqtt"
         }
         for control in config["controls"]:
             key = control["key"]
